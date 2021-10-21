90d8c151b1a73e2093e307d59f9ef0cb8368b215
nkarl <21993921+nkarl@users.noreply.github.com>
2021-10-21 12:20:30 -0700

    added prompts for hw2 and hw3; working on hw1

--- assignments/01/ref/template.c
+++ assignments/01/ref/template.c
@@ -5,66 +5,53 @@
  * Distributed under terms of the GNU LGPL3 license.
  */
 
-#include <stdio.h>
-#include <omp.h>
-#include <stdlib.h>
 #include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+  int N = 100;
 
-int main()
-{
-    int N = 100;
+  int *A = (int *)malloc(N * N * N * sizeof(int));
+  int *B = (int *)malloc(N * N * N * sizeof(int));
+  int *C = (int *)malloc(N * N * N * sizeof(int));
 
-    int *A = (int*) malloc (N * N * N * sizeof(int)) ;
-    int *B = (int*) malloc (N * N * N * sizeof(int)) ;
-    int *C = (int*) malloc (N * N * N * sizeof(int)) ;
+  assert(A != NULL && B != NULL && C != NULL);
 
-    assert(A!=NULL && B!=NULL && C!=NULL);
-
-    //Initialization
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            for (int k = 0; k < N; ++k)
-            {
-                A[i * N * N + j * N + k] = 1;
-                B[i * N * N + j * N + k] = 3;
-                C[i * N * N + j * N + k] = 0;
-            }
-        }
+  // Initialization
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; ++k) {
+        A[i * N * N + j * N + k] = 1;
+        B[i * N * N + j * N + k] = 3;
+        C[i * N * N + j * N + k] = 0;
+      }
     }
+  }
 
-
-
-    // computation - Optimize this part
-    // timer start
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            for (int k = 0; k < N; ++k)
-            {
-                C[i * N * N + j * N + k] = A[i * N * N + j * N + k] + B[i * N * N + j * N + k];
-            }
-        }
+  // computation - Optimize this part
+  // timer start
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; ++k) {
+        C[i * N * N + j * N + k] =
+            A[i * N * N + j * N + k] + B[i * N * N + j * N + k];
+      }
     }
-    // timer end
+  }
+  // timer end
 
-    //print -- Since the output will be too long, just print the sum of all elements
-    int s=0;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            for (int k = 0; k < N; ++k)
-            {
-                s += C[i * N * N + j * N + k];
-            }
-        }
+  // print -- Since the output will be too long, just print the sum of all
+  // elements
+  int s = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; ++k) {
+        s += C[i * N * N + j * N + k];
+      }
     }
+  }
 
-    printf("final sum %d\n", s);
-
-
+  printf("final sum %d\n", s);
 }
--- assignments/01/ref/timer.c
+++ assignments/01/ref/timer.c
@@ -5,26 +5,21 @@
  * Distributed under terms of the GNU LGPL3 license.
  */
 
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
 
-int main()
-{
+int main() {
 
-
-    double start = omp_get_wtime();
-    //timed code
-    {
-        #pragma omp parallel for
-        for (int i = 0; i < 100; ++i)
-        {
-		printf("Hello from thread %d working on iteration %d\n", omp_get_thread_num(), i);
-
-        }
-
+  double start = omp_get_wtime();
+  // timed code
+  {
+#pragma omp parallel for
+    for (int i = 0; i < 100; ++i) {
+      printf("Hello from thread %d working on iteration %d\n",
+             omp_get_thread_num(), i);
     }
-    double end = omp_get_wtime();
-    double time_in_s = end - start;
-    printf("\n\nTime taken is %lfs\n", time_in_s);
-
+  }
+  double end = omp_get_wtime();
+  double time_in_s = end - start;
+  printf("\n\nTime taken is %lfs\n", time_in_s);
 }
